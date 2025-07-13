19f948f3166350e7532a9bf50d8ede7d8dd7553a
tonyking <tony@king.com>
2025-07-14 07:37:55 +1000

    rename half way

--- src/services/code-index/cache-manager.ts
+++ src/services/code-index/cache-manager.ts
@@ -3,8 +3,8 @@ import { createHash } from "crypto"
 import { ICacheManager } from "./interfaces/cache"
 import debounce from "lodash.debounce"
 import { safeWriteJson } from "../../utils/safeWriteJson"
-import { TelemetryService } from "@roo-code/telemetry"
-import { TelemetryEventName } from "@roo-code/types"
+import { TelemetryService } from "@dorjear-code/telemetry"
+import { TelemetryEventName } from "@dorjear-code/types"
 
 /**
  * Manages the cache for code indexing
